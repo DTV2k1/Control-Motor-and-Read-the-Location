0f1d48ee06270bee979da125b22bf73062ad47af
Đỗ Thái Vũ <66055319+DTV2k1@users.noreply.github.com>
2022-05-08 19:44:47 +0700

    Add files via upload

---
+++ Control_Encoder_by_Using_PWM.ino
@@ -0,0 +1,44 @@
+/*
+* Simple code for the L298N motor controller
+* and Metal Gearmotor 37Dx52L mm with 64 CPR Encoder 
+*/
+#include <L298N.h>
+
+// motor pin definition
+#define M_EN 9
+#define M_IN1 8
+#define M_IN2 7
+
+// create a motor instance
+L298N motor(M_EN, M_IN1, M_IN2);
+// PWM signal for the motor driver
+unsigned short driverPwm = 0;
+
+// variables for sending information frequency configuration
+long previousMillis = 0; 
+long interval = 200;  
+
+void setup() {
+  Serial.begin(57600);
+  Serial.println("Basic Motor Test:");
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > interval){
+    Serial.print("PWM value: ");
+    Serial.println(driverPwm);
+    previousMillis = currentMillis;   
+  }
+
+  // check if the data has been sent from the computer:
+  // to make this code work properly, select "No line ending" in monitor
+  if (Serial.available()) {
+    // Narrowing convertion without check! Don't do this in production:)
+    driverPwm = Serial.parseInt();
+  }
+
+  motor.setSpeed(driverPwm);
+  motor.forward();
+}
